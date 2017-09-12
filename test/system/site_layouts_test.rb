6e2ff24e1953636576cc656e607e45d7c3015442
H. Gregor Molter <gregor.molter@secretlab.de>
2017-09-12 10:12:46 +0200

    Added System tests

--- test/system/site_layouts_test.rb
+++ test/system/site_layouts_test.rb
@@ -3,13 +3,30 @@ require "application_system_test_case"
 class SiteLayoutsTest < ApplicationSystemTestCase
   include ApplicationHelper
 
-  test "Home page" do
-    visit root_path 
-    assert_title full_title('')
+  def assert_basics(title)
+    assert_title full_title(title)
     assert_basic_links
     assert_translations
+  end
+
+  test "Home page" do
+    visit root_path 
+    assert_basics('')
     # Test layout for main content
-    #assert_select 'body main div.jumbotron div.container'
+    assert_selector 'body main div.jumbotron div.container'
+  end 
+
+  test "Sign in page" do
+    visit root_path
+    click_link 'Sign in'
+    assert_title full_title('Sign in')
+    assert_basics('')
+    assert_text I18n.t('devise.sessions.sign_in')
+    assert_field User.human_attribute_name(:email), type: 'email'
+    assert_field User.human_attribute_name(:password), type: 'password'
+    assert_field User.human_attribute_name(:remember_me), type: 'checkbox'
+    assert_button I18n.t('devise.sessions.sign_in')
+    assert_link I18n.t('devise.passwords.forgot_password'), href: new_user_password_path()
   end 
 
 end
