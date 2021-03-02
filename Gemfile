84643af0e74c1324a62bdc1e1ba78a3726153d0c
H. Gregor Molter <gregor.molter@secretlab.de>
2021-03-02 22:31:15 +0100

    Updated bootstrap and switched to headless_chrome for system tests.

--- Gemfile
+++ Gemfile
@@ -9,7 +9,7 @@ end
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.0'
 # Bootstrap 4
-gem 'bootstrap', '~> 4.2.1'
+gem 'bootstrap', '~> 4.3.1'
 # Bootstrap 4 forms (currently only from a Work-in-Progress branch)
 gem "bootstrap_form"
 gem 'jquery-rails'
--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,6 +2,6 @@ require "test_helper"
 require "webdrivers"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
- #driven_by :selenium, using: :chrome, screen_size: [1400, 900], options: { args: %w[headless disable-gpu no-sandbox] }
- driven_by :selenium, using: :chrome, screen_size: [1400, 900]
+ driven_by :selenium, using: :headless_chrome, screen_size: [1400, 900]
+ #driven_by :selenium, using: :chrome, screen_size: [1400, 900]
 end
